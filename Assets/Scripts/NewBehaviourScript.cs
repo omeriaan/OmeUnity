9d575a2f7e7918a8b5104fd7409cd8a81aed64c1
Omeriaan <tennessee.suzuki@gmail.com>
2018-05-03 17:27:43 +0900

    [update]もろもろ整理

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var num = 128;
+		Debug.Log(num);
+		Debug.Log("Hello, world!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
